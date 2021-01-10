9fb07ae324073b1f6327032d435fa548fd610056
김경록 <goodboy3421@gmail.com>
2021-01-11 02:48:23 +0900

    refactor: Point 생성자의 접근제한자 private로 변경

--- src/main/java/domain/Point.java
+++ src/main/java/domain/Point.java
@@ -4,7 +4,7 @@ public class Point {
     private final int index;
     private final Direction direction;
 
-    public Point(int index, Direction direction) {
+    private Point(int index, Direction direction) {
         this.index = index;
         this.direction = direction;
     }
