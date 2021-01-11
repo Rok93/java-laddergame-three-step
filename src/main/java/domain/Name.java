a86c0cbe1d08d3b9097004225bb6223d5c8248e6
김경록 <goodboy3421@gmail.com>
2021-01-11 21:36:09 +0900

    feat: 참가자의 이름이 최대 이름길이 제한을 넘으면 예외를 발생시키는 기능

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -1,11 +1,20 @@
 package domain;
 
 public class Name {
+    private static final int MAX_NAME_LENGTH_LIMIT = 5;
+    private static final String NOT_SATISFIED_NAME_LENGTH_ERROR_MESSAGE = "[ERROR] 참가자의 이름은 최소 1글자 이상 5글자 이하만 가능합니다.";
 
     private final String name;
 
     public Name(String name) {
+        validateName(name);
         this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name.length() > MAX_NAME_LENGTH_LIMIT) {
+            throw new IllegalArgumentException(NOT_SATISFIED_NAME_LENGTH_ERROR_MESSAGE);
+        }
     }
 
     public String getValue() {
--- src/test/java/domain/NameTest.java
+++ src/test/java/domain/NameTest.java
@@ -1,17 +1,19 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
     @DisplayName("Name 객체를 생성하는 기능을 테스트한다")
     @ParameterizedTest
     @ValueSource(strings = {
-            "가","가나","가나다", "가나다라", "가나다라마"
+            "가", "가나", "가나다", "가나다라", "가나다라마"
     })
     void testInitName(String nameValue) {
         //when
@@ -19,5 +21,16 @@ class NameTest {
 
         //then
         assertThat(name.getValue()).isEqualTo(nameValue);
+    }
+
+    @DisplayName("최대 이름길이 제한보다 이름의 길이가 길면 예외를 발생시킨다")
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "가나다라마바", "즐라탄 이브라히모비치", "아스필리쿠에타"
+    })
+    void testInitNameIfNameLengthLongerThanMaxNameLengthLimit(String nameValue) {
+        //when //then
+        assertThatThrownBy(() -> new Name(nameValue))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
