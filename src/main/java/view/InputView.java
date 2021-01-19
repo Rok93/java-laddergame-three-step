666e750dd78ef57f38aef5fafd7112dbf417edab
김경록 <goodboy3421@gmail.com>
2021-01-20 02:25:36 +0900

    feat: 사다리의 높이를 입력받는 기능 (view 단)

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,9 +1,6 @@
 package view;
 
-import domain.Name;
-import domain.Names;
-import domain.Result;
-import domain.Results;
+import domain.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +10,7 @@ import java.util.stream.Collectors;
 public class InputView {
     private static final String PLEASE_INPUT_PARTICIPANT_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
     private static final String PLEASE_INPUT_LADDER_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
+    private static final String HOW_MANY_HEIGHT_VALUE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
 
     public static Names inputNames(Scanner scanner) {
         System.out.println(PLEASE_INPUT_PARTICIPANT_NAMES_MESSAGE);
@@ -28,11 +26,26 @@ public class InputView {
     public static Results inputResults(Scanner scanner) {
         System.out.println(PLEASE_INPUT_LADDER_RESULTS_MESSAGE);
         String resultValues = scanner.nextLine();
-        List<Result> results = Arrays.stream(resultValues.split(","))
-                .map(String::trim)
-                .map(Result::new)
-                .collect(Collectors.toList());
+        try {
+            List<Result> results = Arrays.stream(resultValues.split(","))
+                    .map(String::trim)
+                    .map(Result::new)
+                    .collect(Collectors.toList());
 
-        return new Results(results);
+            return new Results(results);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputResults(scanner);
+        }
+    }
+
+    public static Height inputHeight(Scanner scanner) {
+        System.out.println(HOW_MANY_HEIGHT_VALUE_MESSAGE);
+        try {
+            return new Height(scanner.nextInt());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputHeight(scanner);
+        }
     }
 }
