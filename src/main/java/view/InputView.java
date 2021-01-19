21438203472815a6c0f4ec01d89ace203f2d63a2
김경록 <goodboy3421@gmail.com>
2021-01-20 02:21:49 +0900

    feat: 실행 결과들을 입력받는 기능 (view 단)

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,6 +2,8 @@ package view;
 
 import domain.Name;
 import domain.Names;
+import domain.Result;
+import domain.Results;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.stream.Collectors;
 
 public class InputView {
     private static final String PLEASE_INPUT_PARTICIPANT_NAMES_MESSAGE = "참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)";
+    private static final String PLEASE_INPUT_LADDER_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
 
     public static Names inputNames(Scanner scanner) {
         System.out.println(PLEASE_INPUT_PARTICIPANT_NAMES_MESSAGE);
@@ -20,5 +23,16 @@ public class InputView {
                 .collect(Collectors.toList());
 
         return new Names(names);
+    }
+
+    public static Results inputResults(Scanner scanner) {
+        System.out.println(PLEASE_INPUT_LADDER_RESULTS_MESSAGE);
+        String resultValues = scanner.nextLine();
+        List<Result> results = Arrays.stream(resultValues.split(","))
+                .map(String::trim)
+                .map(Result::new)
+                .collect(Collectors.toList());
+
+        return new Results(results);
     }
 }
