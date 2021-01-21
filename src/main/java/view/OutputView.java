bd9a6578fa68dc9897ec21850008072f987d1361
김경록 <goodboy3421@gmail.com>
2021-01-22 01:27:07 +0900

    refactor: 변수명 의미를 파악할 수 있도록 변경

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -33,8 +33,8 @@ public class OutputView {
     }
 
     private static void printLadder(LadderLines ladderLines) {
-        List<LadderLine> ladderLines2 = ladderLines.getValues();
-        ladderLines2.stream()
+        List<LadderLine> ladderLinesValues = ladderLines.getValues();
+        ladderLinesValues.stream()
                 .forEach(OutputView::printLadderLine);
     }
 
