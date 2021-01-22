420c2da082a74330f879d90d0d1c4831885785cb
김경록 <goodboy3421@gmail.com>
2021-01-23 04:32:01 +0900

    refactor: 예외발생시에 예외처리 메시지 출력 및 해당 기능 재실행하도록 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,6 +12,7 @@ public class InputView {
     private static final String PLEASE_INPUT_LADDER_RESULTS_MESSAGE = "실행 결과를 입력하세요. (결과는 쉼표(,)로 구분하세요)";
     private static final String HOW_MANY_HEIGHT_VALUE_MESSAGE = "최대 사다리 높이는 몇 개인가요?";
     private static final String WHO_WANT_TO_SEE_MESSAGE = "결과를 보고 싶은 사람은?";
+    private static final String DELIMITER_COMMA = ",";
 
     private InputView() {
     }
@@ -19,19 +20,24 @@ public class InputView {
     public static Names inputNames(Scanner scanner) {
         System.out.println(PLEASE_INPUT_PARTICIPANT_NAMES_MESSAGE);
         String nameValues = scanner.nextLine();
-        List<Name> names = Arrays.stream(nameValues.split(","))
-                .map(String::trim)
-                .map(Name::new)
-                .collect(Collectors.toList());
+        try {
+            List<Name> names = Arrays.stream(nameValues.split(DELIMITER_COMMA))
+                    .map(String::trim)
+                    .map(Name::new)
+                    .collect(Collectors.toList());
 
-        return new Names(names);
+            return new Names(names);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return inputNames(scanner);
+        }
     }
 
     public static Results inputResults(Scanner scanner) {
         System.out.println(PLEASE_INPUT_LADDER_RESULTS_MESSAGE);
         String resultValues = scanner.nextLine();
         try {
-            List<Result> results = Arrays.stream(resultValues.split(","))
+            List<Result> results = Arrays.stream(resultValues.split(DELIMITER_COMMA))
                     .map(String::trim)
                     .map(Result::new)
                     .collect(Collectors.toList());
@@ -46,7 +52,8 @@ public class InputView {
     public static Height inputHeight(Scanner scanner) {
         System.out.println(HOW_MANY_HEIGHT_VALUE_MESSAGE);
         try {
-            return new Height(scanner.nextInt());
+            String input = scanner.nextLine();
+            return new Height(Integer.parseInt(input));
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             return inputHeight(scanner);
